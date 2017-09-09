68a10e74be2c0b2892ed697d61b8b26b1e5ac346
Chibueze Nelson Ayogu <chibuezeayogu@hotmail.com>
2017-09-09 19:59:13 +0100

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,11 @@
 
 module.exports = {
   development: {
-    use_env_variable: "MORERECIPEDEV",
+    use_env_variable: "MORE_RECIPE_DEV",
     dialet: "postgres"
   },
   test: {
-    use_env_variable: "MORERECIPETEST"
+    use_env_variable: "MORE_RECIPE_TEST"
   },
   production: {
     use_env_variable: "DATABASE_URL"
