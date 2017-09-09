cf33fae9c18e07c57b208d35be3d4135d5c02351
Chibueze Nelson Ayogu <chibuezeayogu@hotmail.com>
2017-09-09 19:56:33 +0100

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,11 @@
 
 module.exports = {
   development: {
-    use_env_variable: "DATABASE_URL",
+    use_env_variable: "More_Recipe",
     dialet: "postgres"
   },
   test: {
-    use_env_variable: "DATABASE_URL""
+    use_env_variable: "More_Recipe_Test"
   },
   production: {
     use_env_variable: "DATABASE_URL"
