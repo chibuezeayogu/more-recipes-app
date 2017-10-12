2314a14c6a23140a6869a49e5617bfa51d9ec8a3
Chibueze Nelson Ayogu <chibuezeayogu@hotmail.com>
2017-10-12 08:04:23 +0100

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,13 @@
 
 module.exports = {
   development: {
-    use_env_variable: "MORE_RECIPE_DEV",
+    use_env_variable: "MORE_RECIPE_DEV"
   },
   test: {
-    use_env_variable: "MORE_RECIPE_TEST",
+    use_env_variable: "MORE_RECIPE_TEST"
   },
   production: {
-    use_env_variable: "DATABASE_URL",
+    use_env_variable: "DATABASE_URL"
     
   }
 };
