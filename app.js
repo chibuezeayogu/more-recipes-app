ef8391be24583187ade53402b88d7ba168fdf18b
Chibueze Nelson Ayogu <chibuezeayogu@hotmail.com>
2017-10-12 23:34:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(expressValidator());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
