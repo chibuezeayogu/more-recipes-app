9752ac7a6cea4830b605fdf0f9f02744c113c4b4
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-11-11 13:04:41 +0100

    add pagination middleware

--- server/helper/pagenate.js
+++ server/helper/pagenate.js
@@ -1,3 +1,10 @@
+/**
+     * @description paginate function
+     * @param {Integer} limit - limit integer
+     * @param {Integer} offset - offset integer
+     * @param {integer} count - count integer
+     * @returns {object} result - result object
+     */
 const paginate = (limit, offset, count) => {
     /** totalCount : total number of records based on query
      * pageCount : total number of pages
@@ -11,7 +18,6 @@ const paginate = (limit, offset, count) => {
     result.currentPage = Math.floor(offset / limit) + 1;
     result.pageCount = Math.ceil(count / limit);
     result.pageSize = Number(limit);
-    
     return result;
   };
 
