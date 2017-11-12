6cce0f2b176e1a30a34892d95c795f73291f730f
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-11-12 16:58:21 +0100

    modify app.js

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+
 import express from 'express';
+import path from 'path';
 import bodyParser from 'body-parser';
 import expressJWT from 'express-jwt';
 import jwt from 'jsonwebtoken';
 import logger from 'morgan';
 import http from 'http';
 import expressValidator from 'express-validator';
+import dotenv from 'dotenv';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
 import routes from './server/routes/index';
+import config from './webpack.config.dev';
+
 
 require('dotenv').config();
-
+const compiler = webpack(config);
 const app = express();
 
 const router = express.Router();
@@ -36,10 +43,17 @@ app.use(logger('dev'));
   message: 'Welcome to More Recipe App',
   }));
 
- app.get('*', (req, res) => res.status(404).send({
-    message: 'Page not found',
+  
+  app.use(require("webpack-dev-middleware")(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
   }));
 
+  app.use(require("webpack-hot-middleware")(compiler));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join( __dirname,'./client/index.html'));
+  });
 
 const port = parseInt(process.env.PORT, 10) || 26000;
 app.set('port', port);
