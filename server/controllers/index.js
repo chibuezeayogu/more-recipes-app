24856c5e00527f718ed12061d5c2d8f47a8ca259
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-11-12 16:54:37 +0100

    remove controllers index.js

--- server/controllers/index.js
+++
@@ -1,8 +0,0 @@
-import userdata from './userdata';
-import review from './reviews';
-import voting from './voting';
-import favourite from './favourites';
-import recipedata from './recipedata';
-
-
-export default { userdata, review, voting, favourite, recipedata };
