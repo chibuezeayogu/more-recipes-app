dd902bd490d079387dcf5094e1874e1ea62b00be
Chibueze Ayogu <chibuezeayogu@hotmail.com>
2018-01-17 19:28:13 +0100

    feat(actionCreators) add action creators and types

--- client/action/actionCreators.js
+++ client/action/actionCreators.js
@@ -1,142 +1,134 @@
-//add recipe
-export const addRecipe = (title, description, ingredients, procedures, imageURL) => {
-    return {
-        type: 'ADD_RECIPE',
-        title, 
-        description, 
-        ingredients, 
-        procedures,
-        imageURL
-    };
-};
+import actionTypes from './actionTypes';
 
-// //modify recipe
-// export const modifyRecipe = (RecipeId) =>{
-//     return {
-//         type: 'MODIFY_RECIPE',
-//         RecipeId
-//     }
-// }
+/**
+ *
+ * @method
+ * @param {String} title - recipe title
+ * @param {String} description - recipe description
+ * @param {String} ingredients - recipe ingredients
+ * @param {String} procedures - recipe procedures
+ * @param {String} imageURL - recipe imageUrl
+ * @returns {Object} payload
+ */
+export const createRecipe =
+  (title, description, ingredients, procedures, imageURL) => ({
+    type: actionTypes.ADD_RECIPE,
+    title,
+    description,
+    ingredients,
+    procedures,
+    imageURL
+  });
 
-// //delete recipe 
-// export const delRecipe = (RecipeId) =>{
-//     return {
-//         type: 'DELETE_RECIPE',
-//         RecipeId
-//     }
-    
-// }
+/**
+ *
+ * @method
+ * @param {Integer} recipeId - recipe id
+ * @returns {Object} payload
+ */
+export const delRecipe = recipeId => ({
+  type: actionTypes.DELETE_RECIPE,
+  recipeId
+});
 
-// //upvote a recipe
-// export const upVoteRecipe = (RecipeId) => {
-//     return {
-//         type: 'UP_VOTE_RECIPE',
-//         RecipeId
-//     }
-    
-// }
+/**
+ *
+ * @method
+ * @param {Integer} recipeId - recipe id
+ * @returns {Object} payload
+ */
+export const upVoteRecipe = recipeId => ({
+  type: actionTypes.UP_VOTE_RECIPE,
+  recipeId
+});
 
-// //downvote a recipe
-// export const downVoteRecipe = (RecipeId) => {
-//     return {
-//         type: 'DOWN_VOTE_RECIPE',
-//         RecipeId
-//     }
-// }
+/**
+ *
+ * @method
+ * @param {Integer} recipeId - recipe id
+ * @returns {Object} payload
+ */
+export const downVoteRecipe = recipeId => ({
+  type: actionTypes.DOWN_VOTE_RECIPE,
+  recipeId
+});
 
-// //add to favorite 
-// export const addToFavourite = (RecipeId) => {
-//     return {
-//         type: 'ADD_TO_FAVOURITE',
-//         RecipeId
-//     }
-// }
+/**
+ *
+ * @method
+ * @param {Integer} recipeId - recipe id
+ * @returns {Object} payload
+ */
+export const addToFavourite = recipeId => ({
+  type: actionTypes.ADD_TO_FAVOURITE,
+  recipeId
+});
 
-// //remove from favourite
-// export const removeFromFavourite = (RecipeId) => {
-//     return {
-//         type: 'REMOVE_FROM_FAVOURITE',
-//         RecipeId
-//     }
-    
-// }
+/**
+ *
+ * @method
+ * @param {Integer} recipeId - recipe id
+ * @returns {Object} payload
+ */
+export const removeFromFavourite = recipeId => ({
+  type: actionTypes.REMOVE_FROM_FAVOURITE,
+  recipeId
+});
 
-//add comment
-export const addComment = (RecipeId, PostedBy, Comment) => {
-    return {
-        type: 'POST_COMMENT',
-        RecipeId,
-        PostedBy,
-        Comment
-    };
-};
+/**
+ *
+ * @method
+ * @param {Integer} recipeId - recipe id
+ * @returns {Object} payload
+ */
+export const getRecipeComment = recipeId => ({
+  type: actionTypes.GET_COMMENTS,
+  recipeId
+});
 
-//del comment
-export const delComment = (RecipeId, PostedBy, Comment) => {
-    return {
-        type: 'DELETE_COMMENT',
-        RecipeId,
-        PostedBy,
-        Comment
-    };
-};
+/**
+ *
+ * @method
+ * @param {Integer} id - recipe id
+ * @param {String} postedBy - user id
+ * @param {Text} comment - user comment
+ * @returns {Object} payload
+ */
+export const postComment = (id, postedBy, comment) => ({
+  type: actionTypes.POST_COMMENT,
+  id,
+  postedBy,
+  comment
+});
 
+/**
+ *
+ * @method
+ * @param {Srting} email - user email
+ * @param {String} password - user password
+ * @returns {Object} payload
+ */
+export const login = (email, password) => ({
+  type: actionTypes.SIGN_IN,
+  email,
+  password
+});
 
-//get all recipe
-export const getAllRecipes = () =>{
-    return {
-        type: 'GET_ALL_RECIPES',
-    };
-};
-
-export const login = (email, password) => {
-    return {
-        type: "SIGN_IN",
-        email,
-        password
-    };
-};
-export const SignOut = () => {
-    return {
-        type: "logout"
-    };
-};
-
-
-
-
-// export const loginSuccess = (data) =>{
-//     return {
-//         type: "SIGN_IN_SUCCESS",
-//         message: data
-//     }
-// }
-
-export const createAccount = (firstname, lastname, email, password, profileImageUrl) => {
-    return {
-        type: 'SIGN_UP',
-        firstname,
-        lastname,
-        email,
-        password,
-        profileImageUrl
-    };
-};
-
-// export const getUser = (userID) => {
-//     return {
-//         type: 'GET_USER',
-//         userId
-//     }
-// }
-
-// export const updateUser = (userID) => {
-//     return {
-//         type: 'UPDATE_USER',
-//         userId
-//     }
-// }
-
-
-
-
+/**
+ * @method
+ * @param {String} firstName - user first name
+ * @param {String} lastName - user last name
+ * @param {String} email - user email
+ * @param {String} password - user password
+ * @param {String} imageUrl - user imageUrl
+ * @returns {Object} payload
+ */
+export const createAccount =
+  (firstName, lastName, email, password, imageUrl) => ({
+    type: actionTypes.SIGN_UP,
+    firstName,
+    lastName,
+    email,
+    password,
+    imageUrl
+  });
