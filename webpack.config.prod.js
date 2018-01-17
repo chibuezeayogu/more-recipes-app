86ce40bb7f7a41021ea754da0418ffdfd27b831f
Chibueze Ayogu <chibuezeayogu@hotmail.com>
2018-01-17 19:06:21 +0100

    feat(signup) create sign-up page make page responsive validate form input fields [Finishes #154273630]

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -18,13 +18,23 @@ export default {
     new webpack.optimize.OccurenceOrderPlugin(),
     new Dotenv(),
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: "'production'"
-      }
+      'process.env.NODE_ENV': JSON.stringify('production')
     }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
-        warnings: false
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true
+      },
+      output: {
+        comments: false
       }
     })
   ],
@@ -66,3 +76,4 @@ export default {
     tls: 'empty'
   },
 };
+
