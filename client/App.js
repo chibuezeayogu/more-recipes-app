7e4668125fe85644cb0315e58254ca346631e21e
Chibueze Nelson Ayogu <chibuezeayogu@hotmail.com>
2018-01-19 07:36:27 +0100

    Update App.js

--- client/App.js
+++ client/App.js
@@ -1,4 +1,5 @@
 // import dependencies
+import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
